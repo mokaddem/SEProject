e69eafb6f5d002daae926b9df29f4d28484e59a3
Alicia MARIN <ma202240@etu.unice.fr>
2015-10-25 14:48:19 +0100

    BDD ressemblerait à ca

--- src/staff/pages/php/BDD.php
+++ src/staff/pages/php/BDD.php
@@ -1,6 +1,30 @@
 <?php
 
+// CLASS A VERIFIER !!!!!!!! Surement des choses à changer
 class BDD extends PDO
 {
-    
+    public static $db = false;
+    private $database_host = '127.0.0.1';
+    private $database_user = 'username';
+    private $database_pass = 'verySecretPassWord';
+    private $database_db = 'database';
+
+    function __construct()
+    {
+        if (self::$db === false) {
+            $this->connect();
+        }
+    }
+
+    private function connect()
+    {
+        $dsn = $this->database_type . ":dbname=" . $this->database_db . ";host=" . $this->database_host;
+        try {
+            self::$db = new PDO($dsn, $this->database_user, $this->database_pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+            self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+
+            //your log handler
+        }
+    }
 }
