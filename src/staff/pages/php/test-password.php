c64159d28310d9088645a4af48806182c05c92dc
Pierre Heymans <pjheymans@gmail.com>
2015-10-28 17:22:02 +0100

    Début de la gestion des extras

--- src/staff/pages/php/test-password.php
+++ src/staff/pages/php/test-password.php
@@ -6,11 +6,11 @@ $db = new BDD();
 $reponse = $db->query('SELECT Password FROM Staff WHERE ID=1 ', $db);
 $donnes = mysql_fetch_array($reponse);
 if ($donnes['Password'] == $_POST['password'] & 1 == $_POST['username']){
-    header("Location: /staff/pages/index.php");
+    header("Location: ../index.php");
     die();
 }
 else{
-    header("Location: /staff/pages/login.php");
+    header("Location: ../login.php");
     die();
 }
 
