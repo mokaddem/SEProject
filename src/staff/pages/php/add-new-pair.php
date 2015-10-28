c8192132388093b4b29086892295908fe48dc955
Sami <sami.mokaddem@student.uclouvain.be>
2015-10-28 17:04:09 +0100

    Added link subscription (one collumn) for the staff. Need to be finished and linked with BDD (*Antoine/Alicia)

--- src/staff/pages/php/add-new-pair.php
+++ src/staff/pages/php/add-new-pair.php
@@ -1,13 +1,40 @@
 <?php
-    $db = new BDD();
+	include_once('BDD.php');
+//	$db = new BDD();
 
-//	$reponse = mysql_query('INSERT INTO `Personne`(`ID`, `FirstName`, `LastName`, `Title`, `ZIPCode`, `PhoneNumber`, `GSMNumber`, `Address`, `BirthDate`, `Mail`, `CreationDate`, `IsPlayer`, `IsOwner`, `IsStaff`) VALUES ('',[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9],[value-10],[value-11],[value-12],[value-13],[value-14])', $db);
+	$database_host = '127.0.0.1';
+	$database_user = 'root';
+	$database_pass = '123';
+	$database_db = 'SEProjectC';
+	$db = new mysqli($database_host, $database_user, $database_pass, $database_db);
+	
+	$req = $db->prepare("INSERT INTO Personne(ID, FirstName, LastName, Title, ZIPCode, PhoneNumber, GSMNumber, Address, BirthDate, Mail, CreationDate, IsPlayer, IsOwner, IsStaff) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-	$req = $db->prepare('INSERT INTO Personne(ID, FirstName, LastName, Title, ZIPCode, PhoneNumber, GSMNumber, Address, BirthDate, Mail, CreationDate, IsPlayer, IsOwner, IsStaff) VALUES(:ID, :FirstName, :LastName, :Title, :ZIPCode, :PhoneNumber, :GSMNumber, :Address, :BirthDate, :Mail, :CreationDate, :IsPlayer, :IsOwner, :IsStaff)');
+//	$req = $db->prepare('INSERT INTO Personne(ID, FirstName, LastName, Title, ZIPCode, PhoneNumber, GSMNumber, Address, BirthDate, Mail, CreationDate, IsPlayer, IsOwner, IsStaff) VALUES('', "bb", "bb", 1, 1234, 12354, 46351, "glkrzjglz e zfzef", 2015-02-02, "lzeijgze@fmezk.com", 2015-02-03, 1, 0, 0)');
 
-	$req->execute(array('ID' => '', 'FirstName' => $_GET['InputPrenom1'], 'LastName' => $_GET['InputNom1'], 'Title' => $_GET['title1'], 'ZIPCode' => $_GET['InputCP1'], 'PhoneNumber' => $_GET['InputFixe1'], 'GSMNumber' => $_GET['InputMob1'], 'Address' => $_GET['InputLoc1'] . "  ," . $_GET['InputAdresse1'] . " n°" . $_GET['InputBat1'] , 'BirthDate' => $_GET['InputBirth1'], 'Mail' => $_GET['InputEmailFirst1'], 'CreationDate' => time(), 'IsPlayer' => 1, 'IsOwner' => 0, 'IsStaff' => 0));
+	$ID	 	= '';
+	$FirstName	= $_GET['InputPrenom2'];
+	$LastName	= $_GET['InputNom2'];
+	$Title		= $_GET['title2'];
+	$ZIPCode	= $_GET['InputCP2'];
+	$PhoneNumber	= $_GET['InputFixe2'];
+	$GSMNumber	= $_GET['InputMob2'];
+	$Address	= $_GET['InputLoc2'] . "  ," . $_GET['InputAdresse2'] . " n°" . $_GET['InputBat2'];
+	$BirthDate	= $_GET['InputBirth2'];
+	$Mail		= $_GET['InputEmailFirst2'];
+	$CreationDate	= time();
+	$IsPlayer	= 1;
+	$IsOwner	= 0;
+	$IsStaff	= 0;
+
+	$req->bind_param("isssissssssiii", $ID, $FirstName, $LastName, $Title, $ZIPCode, $PhoneNumber, $GSMNumber, $Address, $BirthDate, $Mail, $CreationDate, $IsPlayer, $IsOwner, $IsStaff);
+
+	$req->execute();
 
 //	$req->execute(array('ID' => '', 'FirstName' => $_GET['InputPrenom2'], 'LastName' => $_GET['InputNom2'], 'Title' => $_GET['title2'], 'ZIPCode' => $_GET['InputCP2'], 'PhoneNumber' => $_GET['Fixnum2'], 'GSMNumber' => $_GET['Gsmnum2'], 'Address' => $_GET['InputLoc2'] . "  ," . $_GET['InputAdresse2'] . " n°" . $_GET['InputBat2'] , 'BirthDate' => $_GET['InputBirth2'], 'Mail' => $_GET['InputEmailFirst2'], 'CreationDate' => time(), 'IsPlayer' => $_GET['role2'], 'IsOwner' => $_GET['role2'], 'IsStaff' => 0));
 
+
+
+// http://localhost/staff/pages/php/add-new-pair.php?title2=M.&InputNom2=a&InputPrenom2=a&InputBirth2=12%2F12%2F1235&InputAdresse2=fezfvzev&InputBat2=54&InputCP2=35435&InputLoc2=clvkszjvlzekv&InputEmailFirst2=lzefjzf%40ldgzj.com&InputFixe2=%2B335465468&InputMob2=%2B335486846&InputPartNo2=on&Paypal2=on&InputMessage=zarzagezfze&submit=Valider
 
 ?>
