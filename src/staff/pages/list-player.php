45823582ffed38fd33a92e8acd87d32eeac87516
Alicia MARIN <ma202240@etu.unice.fr>
2015-11-03 11:53:00 +0100

    modal

--- src/staff/pages/list-player.php
+++ src/staff/pages/list-player.php
@@ -1,3 +1,5 @@
+<?php require_once("./php/inc/list-player.inc");
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -74,40 +76,27 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <?php require_once("./php/inc/list-player.inc");
-                                            dbAndPrint();
+                                        <?php
+                                            dbAndPrintPlayer();
                                         ?>
                                     </tbody>
                                 </table>
                             </div>
                         </div>
                         <!-- /.panel-body -->
-                        <!-- Modal -->
-                        <div class="modal fade" id="myModal" role="dialog">
-                            <div class="modal-dialog">
-
-                                <!-- Modal content-->
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                        <h4 class="modal-title">Modal Header</h4>
-                                    </div>
-                                    <div class="modal-body">
-                                        <p>Some text in the modal.</p>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
                     </div>
                     <!-- /.panel -->
                 </div>
                 <!-- /.col-lg-6 -->
             </div>
             <!-- /.row -->
+
+            <!-- Modal -->
+            <?php
+                dbAndPrintModal();
+            ?>
+
+
         </div>
         <!-- /#page-wrapper -->
 
