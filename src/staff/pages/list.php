69540286269f386316809b0b3898314d3d1341f0
Alicia MARIN <ma202240@etu.unice.fr>
2015-11-07 21:17:36 +0100

    bouton create dans list.php

--- src/staff/pages/list.php
+++ src/staff/pages/list.php
@@ -61,21 +61,23 @@
                 <div id="page-wrapper">
                     <div class="row">
                         <div class="col-lg-12">
-                            <h1 class="page-header">Liste des <?=$titre?></h1>
+                            <h1 class="page-header">Liste des <?=$titre?>
+                            <a class="btn btn-default btn-outline pull-right" href="./<?=$_GET["type"]?>.php"> Creer</a>
+                            </h1>
                         </div>
                         <!-- /.col-lg-12 -->
                     </div>
                     <!-- /.row -->
                     <div class="row">
                         <div class="col-lg-12">
-                            <div class="panel panel-default">
-                                <!-- <div class="panel-heading">
-                            DataTables Advanced Tables
-                        </div> -->
+                            <!--<div class="panel panel-default">
+                                <div class="panel-heading">
+                                </div>-->
                                 <!-- /.panel-heading -->
                                 <div class="panel-body">
                                     <div class="dataTable_wrapper">
                                         <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+
                                             <thead>
 
                                                 <tr>
