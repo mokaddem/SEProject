14f4da2843d549b39c00e296314fc51912500976
antoinerollin <antoine.rollin26@free.fr>
2015-11-07 16:43:46 +0100

    en -> fr

--- src/staff/pages/php/delete-history.php
+++ src/staff/pages/php/delete-history.php
@@ -5,7 +5,7 @@
 	$db->query('DELETE FROM History');
 
     require_once('add-new-history.php');
-    addHistory(1, 0, "History", "Suppression");
+    addHistory(1, 0, "Historique", "Suppression");
 
 	header("Location: ../index.php");
 ?>
