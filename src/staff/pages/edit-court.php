6c27ae0b5e24265a6c4900c8a2a7a9a834f09a54
Sami <sami.mokaddem@student.uclouvain.be>
2015-11-10 11:54:39 +0100

    Resolved probleme with  and DB

--- src/staff/pages/edit-court.php
+++ src/staff/pages/edit-court.php
@@ -40,6 +40,8 @@
             include("./html/header.php");
 	    include_once('php/BDD.php');
 	    $db = new BDD();
+        $reponse = $db->query('SELECT * FROM Terrain WHERE Terrain.ID ='.$_GET['id']);
+        $dataCourt = $reponse->fetch_array();
         ?>
 
 
@@ -57,34 +59,34 @@
                                 <div class="input-group">
                                     <span class="input-group-addon"><i class="fa fa-circle-thin"></i></span>
                                     <select class="form-control" id="sel1" name="sel1">
-                                        <?php if ($_GET['type'] == "Terre battue")
+                                        <?php if ($dataCourt['Type'] == "Terre battue")
                                             echo "<option selected=\"selected\">Terre battue</option>";
                                             else echo "<option>Terre battue</option>";
                                         ?>
-                                            <?php if ($_GET['type'] == "Gazon")
+                                            <?php if ($dataCourt['Type'] == "Gazon")
                                                 echo "<option selected=\"selected\">Gazon</option>";
                                                 else echo "<option>Gazon</option>";
                                             ?>
-                                            <?php if ($_GET['type'] == "Synthétique")
+                                            <?php if ($dataCourt['Type'] == "Synthétique")
                                                 echo "<option selected=\"selected\">Synthétique</option>";
                                                 else echo "<option>Synthétique</option>";
                                             ?>
                                     </select>
                                     <select class="form-control" id="sel2" name="sel2">
-                                        <?php if ($_GET['etat'] == "Neuf")
+                                        <?php if ($dataCourt['etat'] == "Neuf")
                                             echo "<option selected=\"selected\">Neuf</option>";
                                             else echo "<option>Neuf</option>";
                                         ?>
-                                        <?php if ($_GET['etat'] == "Passable")
+                                        <?php if ($dataCourt['etat'] == "Passable")
                                             echo "<option selected=\"selected\">Passable</option>";
                                             else echo "<option>Passable</option>";
                                         ?>
-                                        <?php if ($_GET['etat'] == "Usé")
+                                        <?php if ($dataCourt['etat'] == "Usé")
                                             echo "<option selected=\"selected\">Usé</option>";
                                             else echo "<option>Usé</option>";
                                         ?>
                                     </select>
-                                    <input type="number" class="form-control" name="surface" id="surface" placeholder="Surface (m²)" min="0" step="1" value="<?php echo $_GET['surface'] ?>" required>
+                                    <input type="number" class="form-control" name="surface" id="surface" placeholder="Surface (m²)" min="0" step="1" value="<?php echo $dataCourt['surface'] ?>" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="sel1"><span class="fa fa-user"></span> Propriétaire</label>
@@ -93,10 +95,11 @@
                                             $reponse = $db->query('SELECT *, Owner.ID as O_id FROM Personne, Owner WHERE Personne.ID = Owner.ID_Personne');
                                             while ($donnes = $reponse->fetch_array())
                                             {
-                                                if ($_GET['O_id'] == $donnes['O_id']){
+                                                if ($dataCourt['ID_Owner'] == $donnes['O_id']){
                                                     echo "<option value=".$donnes['O_id']." selected=\"selected\">".$donnes['FirstName']." ".$donnes['LastName']."</option>";
                                                 }
-                                                echo "<option value=".$donnes['O_id'].">".$donnes['FirstName']." ".$donnes['LastName']."</option>";
+                                                else
+                                                    echo "<option value=".$donnes['O_id'].">".$donnes['FirstName']." ".$donnes['LastName']."</option>";
                                             }
                                         ?>
                                     </select>
@@ -106,16 +109,14 @@
                                     <!--<label for="InputPrenom">Adresse</label>-->
                                     <div class="input-group">
                                         <span class="input-group-addon"><i class="fa fa-map-marker"></i></span>
-                                        <input type="text" class="form-control" id="InputAdresse" name="InputAdresse" placeholder="Adresse" value="<?php echo $_GET['adresse'] ?>" required>
+                                        <input type="text" class="form-control" id="InputAdresse" name="InputAdresse" placeholder="Adresse" value="<?php echo $dataCourt['adresse'] ?>" required>
                                     </div>
                                 </div>
 
                                 <div class="form-group">
                                     <!--<label for="InputMessage">Message</label>-->
                                     <div class="input-group">
-                                        <textarea name="InputNote" id="InputNote" class="form-control" rows="5" required>
-                                            <?php echo $_GET['note'] ?>
-                                        </textarea>
+                                        <textarea name="InputNote" id="InputNote" class="form-control" rows="5" required><?php echo $dataCourt['Note'] ?></textarea>
                                         <span class="input-group-addon"><span class="glyphicon glyphicon-ok"></span></span>
                                     </div>
                                 </div>
