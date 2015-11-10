7063590a400f8e4ccda6349c018af02ee6de6395
antoinerollin <antoine.rollin26@free.fr>
2015-11-10 17:12:28 +0100

    delete player ok - delete owner ok

--- src/staff/pages/php/delete-owner.php
+++ src/staff/pages/php/delete-owner.php
@@ -4,5 +4,9 @@
 
 	$db->query('DELETE FROM Personne WHERE ID='.$_GET['id']);
 	$db->query('DELETE FROM Owner WHERE ID_Personne='.$_GET['id']);
+
+    require_once('add-new-history.php');
+    addHistory( $_GET['id'], "Propriétaire", "Suppression");
+
 	header("Location: ../list-owner.php");
 ?>
--- src/staff/pages/php/delete-player.php
+++ src/staff/pages/php/delete-player.php
@@ -5,6 +5,7 @@
 	$db = new BDD();
 
 	$db->query('DELETE FROM Personne WHERE ID='.$_GET['id']);
+	$db->query('DELETE FROM Player WHERE ID_Personne='.$_GET['id']);
     
     require_once('add-new-history.php');
     addHistory( $_GET['id'], "Joueur", "Suppression");
