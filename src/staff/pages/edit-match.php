33ed06c43f809ae45aa47c5ca0dabe3fe6597794
antoinerollin <antoine.rollin26@free.fr>
2015-11-11 01:18:10 +0100

    edit match well loaded

--- src/staff/pages/edit-match.php
+++ src/staff/pages/edit-match.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Bootstrap Admin Theme</title>
+    <title>Admin Mode - Match</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -39,78 +39,123 @@
         <?php            
             include("./html/header.php");
             include_once('php/BDD.php');
+            
             $db = new BDD();
+            $reponse = $db->query('SELECT * FROM `Match` WHERE '. $_GET['id']. ' = ID');
+            $donnees = $reponse->fetch_array();
+        
+            $t = $db->query('SELECT * FROM `Team` WHERE '.$donnees['ID_Equipe1'].' = ID');
+            $t1 = $t->fetch_array();
+            
+            $t1p = $db->query('SELECT * FROM `Personne` WHERE '.$t1['ID_Player1'].' = ID');
+            $t1p1 = $t1p->fetch_array();
+        
+            $t1p = $db->query('SELECT * FROM `Personne` WHERE '.$t1['ID_Player2'].' = ID');
+            $t1p2 = $t1p->fetch_array();
+        
+            $t1final = $t1p1['FirstName']." ".$t1p1['LastName']." & ".$t1p2['FirstName']." ".$t1p2['LastName'];
+        
+            $t = $db->query('SELECT * FROM `Team` WHERE '.$donnees['ID_Equipe2'].' = ID');
+            $t2 = $t->fetch_array();
+            
+            $t2p = $db->query('SELECT * FROM `Personne` WHERE '.$t2['ID_Player1'].' = ID');
+            $t2p1 = $t2p->fetch_array();
+        
+            $t2p = $db->query('SELECT * FROM `Personne` WHERE '.$t2['ID_Player2'].' = ID');
+            $t2p2 = $t2p->fetch_array();
+        
+            $t2final = $t2p1['FirstName']." ".$t2p1['LastName']." & ".$t2p2['FirstName']." ".$t2p2['LastName'];
+        
+            $t = $db->query('SELECT * FROM `Terrain` WHERE '.$donnees['ID_Terrain'].' = ID');
+            $ter = $t->fetch_array();
+        
+            $terfinal = $ter['adresse'] . " - " . $ter['etat']; 
+             
         ?>
 
 
             <div id="page-wrapper">
                 <div class="row">
                     <div class="col-lg-12 noDeco">
-                        <h1 class="page-header"><a href="list.php?type=match">Liste des matchs > Modifier</a></h1>
+                        <h1 class="page-header"><a href="list.php?type=match"> Liste des matchs</a>> Modifier</h1>
                     </div>
                 </div>
-
                 <!-- Registration form - START -->
                 <div class="row">
-                    <form role="form" method="Get" action="php/edit-match.php">>
+                    <form role="form" method="GET" action="php/inc/edit-match.php">
                         <div class="col-lg-6">
                             <!-- <div class="well well-sm"><strong><span class="glyphicon glyphicon-ok"></span>Required Field</strong></div> -->
 
                             <div class="form-group">
                                 <label for="sel1"><span class="fa fa-users"></span> Première équipe</label>
-                                <select class="form-control" id="sel1" name="sel1">
-                                    <option>[liste des équipes]</option>
-                                   <?php
-                                   /* $reponse = $db->query('');
-                                    while ($donnes = $reponse->fetch_array())
-                                    {
-                                        if ($dataCourt['ID_Owner'] == $donnes['O_id']){
-                                            echo "<option value=".$donnes['O_id']." selected=\"selected\">".$donnes['FirstName']." ".$donnes['LastName']."</option>";
-                                        }
-                                        else
-                                            echo "<option value=".$donnes['O_id'].">".$donnes['FirstName']." ".$donnes['LastName']."</option>";
-                                    }*/
-                                    ?>
+                                <select class="form-control" name="InputEq1" id="InputEq1">
+                                    <?php                  
+    echo '<option value="'.$donnees['ID_Equipe1'].'" selected="'.$t1final.'">'.$t1final.'</option>'; ?>
+                                
+                                <?php
+										$db = new BDD();
+										$reponse = $db->query('SELECT * FROM Team');
+										while ($donnes = $reponse->fetch_array())
+										{
+                                            $p = $db->query('SELECT * FROM Personne WHERE '.$donnes['ID_Player1'].' = ID');
+                                            $p1 = $p->fetch_array();
+                                            $p = $db->query('SELECT * FROM Personne WHERE '.$donnes['ID_Player2'].' = ID');
+											$p2 = $p->fetch_array();
+                                            echo "<option value=".$donnes['ID'].">".$p1['FirstName']." ".$p1['LastName']." & ".$p2['FirstName']." ".$p2['LastName']."</option>";
+										}
+			 	    				?>
                                 </select>
                             </div>
 
                             <div class="form-group">
                                 <label for="sel1"><span class="fa fa-users"></span> Seconde équipe</label>
-                                <select class="form-control" id="sel2" name="sel2">
-                                    <option>[liste des équipes]</option>
-
+                                <select class="form-control" name="InputEq2" id="InputEq2">
+                                    <?php                  
+    echo '<option value="'.$donnees['ID_Equipe2'].'" selected="'.$t2final.'">'.$t2final.'</option>'; ?>
+                                    <?php
+										$db = new BDD();
+										$reponse = $db->query('SELECT * FROM Team');
+										while ($donnes = $reponse->fetch_array())
+										{
+                                            $p = $db->query('SELECT * FROM Personne WHERE '.$donnes['ID_Player1'].' = ID');
+                                            $p1 = $p->fetch_array();
+                                            $p = $db->query('SELECT * FROM Personne WHERE '.$donnes['ID_Player2'].' = ID');
+											$p2 = $p->fetch_array();
+                                            echo "<option value=".$donnes['ID'].">".$p1['FirstName']." ".$p1['LastName']." & ".$p2['FirstName']." ".$p2['LastName']."</option>";
+										}
+			 	    				?>
                                 </select>
                             </div>
 
                             <div class="form-group">
                                 <label for="sel1"><span class="fa fa-map-marker"></span> Lieu</label>
-                                <select class="form-control" id="sel3" name="sel3">
-                                    <option>[liste des terrains]</option>
+                                
+                                <select class="form-control" name="InputCourt" id="InputCourt">
+                                    <?php                  
+    echo '<option value="'.$donnees['ID_Terrain'].'" selected="'.$terfinal.'">'.$terfinal.'</option>'; ?>
                                     <?php
-                                    $reponse = $db->query('SELECT Terrain.ID, adresse, FirstName, LastName, etat, `Type`, Note, surface FROM Terrain, Personne, Owner WHERE Terrain.ID_Owner = Owner.ID and Personne.ID = Owner.ID_Personne');
-                                    while ($donnes = $reponse->fetch_array())
-                                    {
-                                        if ($_GET['id'] == $donnes['ID']){
-                                            echo "<option value=".$donnes['ID']." selected=\"selected\">".$donnes['ID']." - ".$donnes['FirstName']. " " . $donnes['LastName'] . " - " . $donnes['Type'] . " - " . $donnes['etat'] . $donnes['Node'] . "</option>";
-                                        }
-                                        else
-                                            echo "<option value=".$donnes['ID']." >".$donnes['ID']." - ".$donnes['FirstName']. " " . $donnes['LastName'] . " - " . $donnes['Type'] . " - " . $donnes['etat'] . $donnes['Node'] . "</option>";
-                                    }
-                                    ?>
-
+										$db = new BDD();
+										$reponse = $db->query('SELECT * FROM Terrain');
+										while ($donnes = $reponse->fetch_array())
+										{
+											echo "<option value=".$donnes['ID'].">".$donnes['adresse']." - ".$donnes['etat']."</option>";
+										}
+			 	    				?>
                                 </select>
                             </div>
                             <div class="form-group">
                                 <label for="sel1"><span class="fa fa-clock-o"></span> Date & Heure</label>
                                 <div class="input-group">
                                     <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
-                                    <input type="date" min="2015-10-10" max="2048-10-10" name="calendar" value="2015-10-10">
-                                    <input type="time" name="time" value="08:00">
+                                    
+                                    <?php 
+                                        echo '<input type="date" min="'.date("Y-m-d").'" max="2048-10-10" id="InputDate" name="InputDate" value="'.$donnees['date'].'">';
+                                        echo '<input type="time" id="InputHour" name="InputHour" value="'.$donnees['hour'].'">';
+                                    ?>
                                 </div>
                             </div>
 
-                            <a class="btn btn-info" href="list.php?type=match">Retour</a>
-                            <input type="submit" name="edit-submit" id="edit-submit" value="Sauvegarder" class="btn btn-success pull-right">
+                            <input type="submit" name="submit" id="submit" value="Créer" class="btn btn-info pull-right">
 
                         </div>
                     </form>
