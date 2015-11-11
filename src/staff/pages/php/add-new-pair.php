cb94d2ed84a44f9d8715def5b211e23c006d1ff7
antoinerollin <antoine.rollin26@free.fr>
2015-11-11 14:10:38 +0100

    when add pair -> add team

--- src/staff/pages/php/add-new-pair.php
+++ src/staff/pages/php/add-new-pair.php
@@ -14,71 +14,93 @@
 
 //	$req = $db->prepare('INSERT INTO Personne(ID, FirstName, LastName, Title, ZIPCode, PhoneNumber, GSMNumber, Address, BirthDate, Mail, CreationDate, IsPlayer, IsOwner, IsStaff) VALUES('', "bb", "bb", 1, 1234, 12354, 46351, "glkrzjglz e zfzef", 2015-02-02, "lzeijgze@fmezk.com", 2015-02-03, 1, 0, 0)');
 
-	$ID	 	= '';
-	$FirstName	= $_GET['InputPrenom1'];
-	$LastName	= $_GET['InputNom1'];
-	$Title		= $_GET['title1'];
-	$ZIPCode	= $_GET['InputCP1'];
-	$PhoneNumber	= $_GET['InputFixe1'];
-	$GSMNumber	= $_GET['InputMob1'];
-	$Ville		= $_GET['InputLoc1'];
-	$Rue		= $_GET['InputAdresse1'];
-	$Number		= $_GET['InputBat1'];
-	$BirthDate	= $_GET['birth_year1']."-".$_GET['birth_month1']."-".$_GET['birth_day1'];
-	$Mail		= $_GET['InputEmailFirst1'];
-	$CreationDate	= date('Y-m-d');
-	$IsPlayer	= 1;
-	$IsOwner	= 0;
-	$IsStaff	= 0;
+	$ID1	 	= '';
+	$FirstName1	= $_GET['InputPrenom1'];
+	$LastName1	= $_GET['InputNom1'];
+	$Title1	= $_GET['title1'];
+	$ZIPCode1	= $_GET['InputCP1'];
+	$PhoneNumber1	= $_GET['InputFixe1'];
+	$GSMNumber1	= $_GET['InputMob1'];
+	$Ville1		= $_GET['InputLoc1'];
+	$Rue1		= $_GET['InputAdresse1'];
+	$Number1		= $_GET['InputBat1'];
+	$BirthDate1	= $_GET['birth_year1']."-".$_GET['birth_month1']."-".$_GET['birth_day1'];
+	$Mail1		= $_GET['InputEmailFirst1'];
+	$CreationDate = date('Y-m-d');
+	$IsPlayer1	= 1;
+	$IsOwner1	= 0;
+	$IsStaff1	= 0;
 
     
 
-	$req->bind_param("issiissssisssiii", $ID, $FirstName, $LastName, $Title, $ZIPCode, $PhoneNumber, $GSMNumber, $Ville, $Rue, $Number, $BirthDate, $Mail, $CreationDate, $IsPlayer, $IsOwner, $IsStaff);
+	$req->bind_param("issiissssisssiii", $ID1, $FirstName1, $LastName1, $Title1, $ZIPCode1, $PhoneNumber1, $GSMNumber1, $Ville1, $Rue1, $Number1, $BirthDate1, $Mail1, $CreationDate, $IsPlayer1, $IsOwner1, $IsStaff1);
 
 	$req->execute();
 
 
-    $reponse = $db->query('SELECT * FROM Personne WHERE "'.$FirstName.'" = FirstName AND "'.$LastName.'" = LastName');
-    $donnees = $reponse->fetch_array();
-    addHistory( $donnees["ID"], "Joueur", "Ajout");
+    $reponse = $db->query('SELECT * FROM Personne WHERE "'.$FirstName1.'" = FirstName AND "'.$LastName1.'" = LastName');
+    $donnees1 = $reponse->fetch_array();
+    addHistory( $donnees1["ID"], "Joueur", "Ajout");
 	
     
 
 	$req = $db->prepare("INSERT INTO Personne(ID, FirstName, LastName, Title, ZIPCode, PhoneNumber, GSMNumber, Rue, Number, Ville, BirthDate, Mail, CreationDate, IsPlayer, IsOwner, IsStaff) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-	$ID	 	    = '';
-	$FirstName	= $_GET['InputPrenom2'];
-	$LastName	= $_GET['InputNom2'];
-	$Title		= $_GET['title2'];
-	$ZIPCode	= $_GET['InputCP2'];
-	$PhoneNumber= $_GET['InputFixe2'];
-	$GSMNumber	= $_GET['InputMob2'];
-	$Ville		= $_GET['InputLoc2'];
-	$Rue		= $_GET['InputAdresse2'];
-	$Number		= $_GET['InputBat2'];
-	$BirthDate	= $_GET['birth_year2']."-".$_GET['birth_month2']."-".$_GET['birth_day2'];
-	$Mail		= $_GET['InputEmailFirst2'];
+	$ID2	 	    = '';
+	$FirstName2	= $_GET['InputPrenom2'];
+	$LastName2	= $_GET['InputNom2'];
+	$Title2		= $_GET['title2'];
+	$ZIPCode2	= $_GET['InputCP2'];
+	$PhoneNumber2= $_GET['InputFixe2'];
+	$GSMNumber2	= $_GET['InputMob2'];
+	$Ville2		= $_GET['InputLoc2'];
+	$Rue2		= $_GET['InputAdresse2'];
+	$Number2		= $_GET['InputBat2'];
+	$BirthDate2	= $_GET['birth_year2']."-".$_GET['birth_month2']."-".$_GET['birth_day2'];
+	$Mail2		= $_GET['InputEmailFirst2'];
 	//$CreationDate	= date('Y-m-d');
-	$IsPlayer	= 1;
-	$IsOwner	= 0;
-	$IsStaff	= 0;
+	$IsPlayer2	= 1;
+	$IsOwner2	= 0;
+	$IsStaff2	= 0;
 
-	$req->bind_param("issiissssisssiii", $ID, $FirstName, $LastName, $Title, $ZIPCode, $PhoneNumber, $GSMNumber, $Ville, $Rue, $Number, $BirthDate, $Mail, $CreationDate, $IsPlayer, $IsOwner, $IsStaff);
+	$req->bind_param("issiissssisssiii", $ID2, $FirstName2, $LastName2, $Title2, $ZIPCode2, $PhoneNumber2, $GSMNumber2, $Ville2, $Rue2, $Number2, $BirthDate2, $Mail2, $CreationDate, $IsPlayer2, $IsOwner2, $IsStaff2);
 
 	$req->execute();
 
 
-    $reponse = $db->query('SELECT * FROM Personne WHERE "'.$FirstName.'" = FirstName AND "'.$LastName.'" = LastName');    
+    $reponse = $db->query('SELECT * FROM Personne WHERE "'.$FirstName2.'" = FirstName AND "'.$LastName2.'" = LastName');    
+    $donnees2 = $reponse->fetch_array();
+    addHistory( $donnees2["ID"], "Joueur", "Ajout");
+
+    
+
+    // -----------------------------------------------
+	$req = $db->prepare("INSERT INTO Team(ID, ID_player1, ID_player2, ID_Cat, NbWinMatch) VALUES(?, ?, ?, ?, ?)");
+
+//	$req = $db->prepare('INSERT INTO Personne(ID, FirstName, LastName, Title, ZIPCode, PhoneNumber, GSMNumber, Address, BirthDate, Mail, CreationDate, IsPlayer, IsOwner, IsStaff) VALUES('', "bb", "bb", 1, 1234, 12354, 46351, "glkrzjglz e zfzef", 2015-02-02, "lzeijgze@fmezk.com", 2015-02-03, 1, 0, 0)');
+
+	$ID	 	= '';
+	$ID_player1	= $donnees1['ID'];
+	$ID_player2	= $donnees2['ID'];
+	$ID_Cat		= '1';
+	$NbmatchWin	= '0';
+
+	$req->bind_param("iiiii", $ID, $ID_player1, $ID_player2, $ID_Cat, $NbmatchWin);
+
+	$req->execute();
+
+    $reponse = $db->query('SELECT * FROM Team WHERE '.$ID_player1.' = ID_Player1 AND '.$ID_player2.' = ID_Player2');
     $donnees = $reponse->fetch_array();
-    addHistory( $donnees["ID"], "Joueur", "Ajout");
-
-	if (array_key_exists($_SESSION)) {
-		header("Location: ../list.php?type=player");
-	} else {
-		header( "refresh:5;url=../../../../index.php?action=register" );
-	}
-
-//	$req->execute(array('ID' => '', 'FirstName' => $_GET['InputPrenom2'], 'LastName' => $_GET['InputNom2'], 'Title' => $_GET['title2'], 'ZIPCode' => $_GET['InputCP2'], 'PhoneNumber' => $_GET['Fixnum2'], 'GSMNumber' => $_GET['Gsmnum2'], 'Address' => $_GET['InputLoc2'] . "  ," . $_GET['InputAdresse2'] . " n°" . $_GET['InputBat2'] , 'BirthDate' => $_GET['InputBirth2'], 'Mail' => $_GET['InputEmailFirst2'], 'CreationDate' => time(), 'IsPlayer' => $_GET['role2'], 'IsOwner' => $_GET['role2'], 'IsStaff' => 0));
 	
+    addHistory( $donnees["ID"], "Equipe", "Ajout");
+
+    // -----------------------------------------------
+
+	/*if (array_key_exists($_SESSION)) {*/
+	header("Location: ../list.php?type=player");
+	/*} else {
+		header( "refresh:5;url=../../../../index.php?action=register" );
+    }*/
+
 
 ?>
--- src/staff/pages/player.php
+++ src/staff/pages/player.php
@@ -107,7 +107,7 @@
                                         <select name='birth_year1' id='birth_year1'>
                                             <option value="na">Année</option>
                                             <?php
-                                                  for ($i = date(Y); $i >= 1900; $i--) {
+                                                  for ($i = date('Y'); $i >= 1900; $i--) {
                                                     echo "<option>$i</option>\n";
                                                   }
                                             ?>
@@ -245,7 +245,7 @@
                                         <select name='birth_year2' id='birth_year2'>
                                             <option value="na">Année</option>
                                             <?php
-                                                  for ($i = date(Y); $i >= 1900; $i--) {
+                                                  for ($i = date('Y'); $i >= 1900; $i--) {
                                                     echo "<option>$i</option>\n";
                                                   }
                                             ?>
