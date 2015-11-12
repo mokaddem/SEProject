a8fb2cfad8577e362c3b5db02cfb6ee84a42a2ac
Sami <sami.mokaddem@student.uclouvain.be>
2015-11-12 13:05:23 +0100

    started input score for poule: MatchVS and choose equipe done

--- src/staff/pages/input-group-score.php
+++ src/staff/pages/input-group-score.php
@@ -42,10 +42,23 @@
 
     <div id="wrapper">
 
-        <?php            
+        <?php
             include("./html/header.php");
+            include_once('php/BDD.php');
+
+
+            $db = new BDD();
+
+            $grpSattmp = $db->query('SELECT * FROM GroupSaturday');
+            $grpSuntmp = $db->query('SELECT * FROM GroupSunday');
+
+      //      $ListMatch = $db->prepare('SELECT * FROM \'Match\' WHERE \'Match\'.Group_ID = ?')
+            $PouleID=9;
+            $TeamID=46;
         ?>
 
+
+        <script>console.log("<?php  ?>")</script>
             <div id="page-wrapper">
                 <div class="row">
                     <div class="col-lg-12">
@@ -57,19 +70,54 @@
 
                 <div class="form-group">
                     <label for="sel1"><span class="fa fa-dot-circle-o"></span> Choix de la poule</label>
-                    <select class="form-control" id="sel1">
-                        <option>[liste des poules]</option>
-                        <!-- <option>propriétaire</option> -->
+                    <select class="form-control" id="selectedPoule" name="selectedPoule">
+                        <?php
+                            while ($row = $grpSattmp->fetch_array())
+                            {
+                                echo "<option value=".$row['ID'].">".$row['ID']."</option>";
+                            }
+                        ?>
                     </select>
                 </div>
 
                 <div class="form-group">
-                    <label for="sel1"><span class="fa fa-dot-circle-o"></span> Choix du match</label>
-                    <select class="form-control" id="sel1">
-                        <option>[liste des matchs de la poule]</option>
-                        <!-- <option>propriétaire</option> -->
+                    <label for="sel1"><span class="fa fa-dot-circle-o"></span> Choix de l'équipe</label>
+                    <select class="form-control" id="selectedPoule" name="selectedPoule">
+                    <?php
+                        $reponse = $db->query('SELECT * FROM Team, GroupSaturday WHERE GroupSaturday.ID='.$PouleID.' AND (Team.ID=GroupSaturday.ID_t1 OR Team.ID=GroupSaturday.ID_t2 OR Team.ID=GroupSaturday.ID_t3 OR Team.ID=GroupSaturday.ID_t4 OR Team.ID=GroupSaturday.ID_t5)');
+                        while ($donnes = $reponse->fetch_array())
+                        {
+                            $p = $db->query('SELECT * FROM Personne WHERE '.$donnes['ID_Player1'].' = ID');
+                            $p1 = $p->fetch_array();
+                            $p = $db->query('SELECT * FROM Personne WHERE '.$donnes['ID_Player2'].' = ID');
+                            $p2 = $p->fetch_array();
+                            echo "<option value=".$donnes['ID'].">".$p1['FirstName']." ".$p1['LastName']." & ".$p2['FirstName']." ".$p2['LastName']."</option>";
+                        }
+                    ?>
                     </select>
                 </div>
+
+                <div class="form-group">
+                <label for="sel1"><span class="fa fa-dot-circle-o"></span> Matchs VS</label>
+                    <?php
+                    $reponse = $db->query('SELECT FirstName, LastName, R2.ID FROM (SELECT * FROM (SELECT ID_Equipe1, ID_Equipe2 FROM `Match`, Team WHERE `Match`.Poule_ID ='.$PouleID.' AND `Match`.ID_Equipe1 ='.$TeamID.' OR `Match`.ID_Equipe2 ='.$TeamID.' GROUP BY ID_Equipe1 ) AS R1, Team WHERE R1.ID_Equipe1 = Team.ID OR R1.ID_Equipe2 = Team.ID GROUP BY Team.ID ) AS R2, Personne WHERE R2.ID_Player1 = Personne.ID OR R2.ID_Player2 = Personne.ID');
+                    while ($donnes = $reponse->fetch_array())
+                    {
+                        $p1 = $donnes['FirstName'] . " " . $donnes['LastName'];
+                        $donnes = $reponse->fetch_array();
+                        $p2 = $donnes['FirstName'] . " " . $donnes['LastName'];
+                        ?>
+                        <div class="input-group">
+                        <span class="input-group-addon"><i></i><?=$p1." & ".$p2?></span>
+                        <input type="number" class="form-control" name="size" id="size" placeholder="0" min="0" step="1"  style="width: 60px;" required>
+                        <input type="number" class="form-control" name="size" id="size" placeholder="0" min="0" step="1"  style="width: 60px;" required>
+                    </div>
+                   <?php }
+                    ?>
+                </div>
+
+
+
 
                 <div class="form-group">
                     <label for="sel1"><span class="fa fa-edit"></span> Vainqueur</label>
@@ -115,6 +163,19 @@
             });
         });
     </script>
+
+    <script type="text/javascript">
+        function refreshMatchs(){
+            for (i = 1; i < document.getElementById("size").value; i++) {
+            var mydiv = document.getElementById('mydiv');
+            var mydiv2 = mydiv.cloneNode(true);
+            mydiv.appendChild(mydiv2);
+            }
+        }
+    </script>
+
+    <script type="text/javascript"> document.getElementById("selectedPoule").addEventListener("change", refreshMatchs);</script>
+
 
 </body>
 
