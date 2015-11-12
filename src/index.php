d900e15efe0ddefd6e10406b9caf445b5a3d71b0
antoinerollin <antoine.rollin26@free.fr>
2015-11-12 12:25:04 +0100

    icon page on index

--- src/index.php
+++ src/index.php
@@ -14,6 +14,9 @@
          ?>
 
         <head lang="fr">
+            
+            <link rel="shortcut icon" href="./images/icon.ico">
+            
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
