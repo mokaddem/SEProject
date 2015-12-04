c2e3c8a36755b782ce97f33effb1e3e1bb60626f
System Administrator <root@MacBook-Pro-de-Florian.local>
2015-12-04 01:18:30 +0100

    Formulaire de Contact + bouton des groupes (reste à modifier groupe quand je pourrai) Attention avec le bug de chemin absolu, à montrer !

---
+++ src/staff/pages/contact_Staff.php
@@ -0,0 +1,329 @@
+<?php
+	include_once('php/BDD.php');
+	include "../../mail/mail_helper.php";
+	
+	$db = BDconnect();
+	
+	// on récupère le contenu des variables
+	//Champs
+	$dest = isset($_POST['dest'])?$_POST['dest']:'';	
+	$sujet = isset($_POST['sujet'])?$_POST['sujet']:'';
+	$message = isset($_POST['message'])?$_POST['message']:'';
+	//CheckBox Desti
+	$parti=isset($_POST['parti'])?$_POST['parti']:'';
+	$propri=isset($_POST['proprio'])?$_POST['proprio']:'';
+	$leaderSam = isset($_POST['leaderSam'])?$_POST['leaderSam']:'';
+	$npSam=isset($_POST['NPSam'])?$_POST['NPSam']:'';
+	$tousSam=isset($_POST['tousSam'])?$_POST['tousSam']:'';
+	$leaderDim = isset($_POST['leaderDim'])?$_POST['leaderDim']:'';
+	$npDim=isset($_POST['NPDim'])?$_POST['NPDim']:'';
+	$tousDim=isset($_POST['tousDim'])?$_POST['tousDim']:'';
+	//CheckBox Message
+	$mesLeader=isset($_POST['mesLeader'])?$_POST['mesLeader']:'';
+	$mesTous=isset($_POST['mesTous'])?$_POST['mesTous']:'';
+	$mesNP=isset($_POST['mesNP'])?$_POST['mesNP']:'';
+	
+//On gère les destinataires
+//////////////////////////////////////////////////////////////////////////////	
+	//On calcul le champ dest pour pouvoir s'en servir après 
+	$destCoupe = explode(",",$dest);
+	
+	
+	//Pour calculer le nombre de catégorie et vérifier si elles ont été recue
+	$categorie = $db->query('SELECT Designation FROM Categorie');
+    $listCat;
+    $z=0;
+    $cat="cat_";
+    while($lcat = $categorie -> fetch_array())
+    {
+    	$listCat[$z] = $lcat['Designation'];
+    	$z++;
+    }
+    $z--;
+    for($z;$z!=-1;$z--)
+    {
+    	$str=(string) $z;
+    	$cat.=$str;
+    	$catish=isset($_POST[$cat])?$_POST[$cat]:'';
+    	if($catish==true)
+    	{
+    		$catRecu = $db->query('SELECT DISTINCT Mail FROM Personne JOIN Team ON Team.ID_Player1 = Personne.ID OR Team.ID_Player2 = Personne.ID JOIN Categorie ON Team.ID_Cat = Categorie.ID WHERE Categorie.Designation="'.$listCat[$z].'"');
+    		$listcatRecu;
+    		$compteur=0;
+    		while($lstRecu = $catRecu -> fetch_array())
+    		{
+    			$listcatRecu[$compteur] = $lstRecu['Mail'];
+    			$compteur++;
+    		}
+    		$ajoutCat=count($destCoupe);    		
+    		$compteur--;
+    		for($compteur;$compteur!=-1;$compteur--)
+    		{
+    			$destCoupe[$ajoutCat]=$listcatRecu[$compteur];
+    			$ajoutCat++;
+    		}
+    	}
+    	$str="";
+    	$cat="cat_";
+    }
+    
+///////////////////////////////////////////////////////////////////////////////    
+   //Si tous les participants sont à joindre
+	if($parti==true)
+	{
+		$adresse = $db->query('SELECT Mail FROM Personne WHERE IsPlayer=1');
+		$listAdr;
+		$i=0;
+		while($ladr = $adresse -> fetch_array())
+		{
+			$listAdr[$i] = $ladr['Mail'];
+			$i++;
+		}
+		$ajout=count($destCoupe);
+		$i--;
+		for($i;$i!=-1;$i--)
+		{
+			$destCoupe[$ajout]=$listAdr[$i];
+			$ajout++;
+		}	
+	}
+///////////////////////////////////////////////////////////////////////////////
+	if($propri==true)
+	{
+		$adresse = $db->query('SELECT Mail FROM Personne WHERE IsOwner=1');
+		$listAdr;
+		$i=0;
+		while($ladr = $adresse -> fetch_array())
+		{
+			$listAdr[$i] = $ladr['Mail'];
+			$i++;
+		}
+		$ajout=count($destCoupe);
+		$i--;
+		for($i;$i!=-1;$i--)
+		{
+			$destCoupe[$ajout]=$listAdr[$i];
+			$ajout++;
+		}	
+	}
+///////////////////////////////////////////////////////////////////////////////
+if($leaderSam==true)
+	{
+		$adresse = $db->query('SELECT DISTINCT Mail FROM Personne JOIN Player ON Player.ID_Personne = Personne.ID JOIN Team ON Team.ID_Player1=Player.ID_Personne OR Team.ID_Player2=Player.ID_Personne JOIN GroupSaturday ON GroupSaturday.ID_t1 = Team.ID OR GroupSaturday.ID_t2 = Team.ID OR GroupSaturday.ID_t3 = Team.ID OR GroupSaturday.ID_t4 = Team.ID OR GroupSaturday.ID_t5 = Team.ID OR GroupSaturday.ID_t6 = Team.ID OR GroupSaturday.ID_t7 = Team.ID OR GroupSaturday.ID_t8 = Team.ID WHERE Player.IsLeader=1');
+		$listAdr;
+		$i=0;
+		while($ladr = $adresse -> fetch_array())
+		{
+			$listAdr[$i] = $ladr['Mail'];
+			$i++;
+		}
+		$ajout=count($destCoupe);
+		$i--;
+		for($i;$i!=-1;$i--)
+		{
+			$destCoupe[$ajout]=$listAdr[$i];
+			$ajout++;
+		}
+	}
+///////////////////////////////////////////////////////////////////////////////
+if($leaderDim==true)
+	{
+		$adresse = $db->query('SELECT DISTINCT Mail FROM Personne JOIN Player ON Player.ID_Personne = Personne.ID JOIN Team ON Team.ID_Player1=Player.ID_Personne OR Team.ID_Player2=Player.ID_Personne JOIN GroupSunday ON GroupSunday.ID_t1 = Team.ID OR GroupSunday.ID_t2 = Team.ID OR GroupSunday.ID_t3 = Team.ID OR GroupSunday.ID_t4 = Team.ID OR GroupSunday.ID_t5 = Team.ID OR GroupSunday.ID_t6 = Team.ID OR GroupSunday.ID_t7 = Team.ID OR GroupSunday.ID_t8 = Team.ID WHERE Player.IsLeader=1');
+		$listAdr;
+		$i=0;
+		while($ladr = $adresse -> fetch_array())
+		{
+			$listAdr[$i] = $ladr['Mail'];
+			$i++;
+		}
+		$ajout=count($destCoupe);
+		$i--;
+		for($i;$i!=-1;$i--)
+		{
+			$destCoupe[$ajout]=$listAdr[$i];
+			$ajout++;
+		}	
+	}
+///////////////////////////////////////////////////////////////////////////////
+if($npSam==true)
+	{
+		$adresse = $db->query('SELECT DISTINCT Mail FROM Personne JOIN Player ON Player.ID_Personne = Personne.ID JOIN Team ON Team.ID_Player1=Player.ID_Personne OR Team.ID_Player2=Player.ID_Personne JOIN GroupSaturday ON GroupSaturday.ID_t1 = Team.ID OR GroupSaturday.ID_t2 = Team.ID OR GroupSaturday.ID_t3 = Team.ID OR GroupSaturday.ID_t4 = Team.ID OR GroupSaturday.ID_t5 = Team.ID OR GroupSaturday.ID_t6 = Team.ID OR GroupSaturday.ID_t7 = Team.ID OR GroupSaturday.ID_t8 = Team.ID WHERE Player.Paid=0');
+		$listAdr;
+		$i=0;
+		while($ladr = $adresse -> fetch_array())
+		{
+			$listAdr[$i] = $ladr['Mail'];
+			$i++;
+		}
+		$ajout=count($destCoupe);
+		$i--;
+		for($i;$i!=-1;$i--)
+		{
+			$destCoupe[$ajout]=$listAdr[$i];
+			$ajout++;
+		}	
+	}
+///////////////////////////////////////////////////////////////////////////////
+if($npDim==true)
+	{
+		$adresse = $db->query('SELECT DISTINCT Mail FROM Personne JOIN Player ON Player.ID_Personne = Personne.ID JOIN Team ON Team.ID_Player1=Player.ID_Personne OR Team.ID_Player2=Player.ID_Personne JOIN GroupSunday ON GroupSunday.ID_t1 = Team.ID OR GroupSunday.ID_t2 = Team.ID OR GroupSunday.ID_t3 = Team.ID OR GroupSunday.ID_t4 = Team.ID OR GroupSunday.ID_t5 = Team.ID OR GroupSunday.ID_t6 = Team.ID OR GroupSunday.ID_t7 = Team.ID OR GroupSunday.ID_t8 = Team.ID WHERE Player.Paid=0');
+		$listAdr;
+		$i=0;
+		while($ladr = $adresse -> fetch_array())
+		{
+			$listAdr[$i] = $ladr['Mail'];
+			$i++;
+		}
+		$ajout=count($destCoupe);
+		$i--;
+		for($i;$i!=-1;$i--)
+		{
+			$destCoupe[$ajout]=$listAdr[$i];
+			$ajout++;
+		}	
+	}
+///////////////////////////////////////////////////////////////////////////////
+if($tousSam==true)
+	{
+		$adresse = $db->query('SELECT DISTINCT Mail FROM Personne JOIN Player ON Player.ID_Personne = Personne.ID JOIN Team ON Team.ID_Player1=Player.ID_Personne OR Team.ID_Player2=Player.ID_Personne JOIN GroupSaturday ON GroupSaturday.ID_t1 = Team.ID OR GroupSaturday.ID_t2 = Team.ID OR GroupSaturday.ID_t3 = Team.ID OR GroupSaturday.ID_t4 = Team.ID OR GroupSaturday.ID_t5 = Team.ID OR GroupSaturday.ID_t6 = Team.ID OR GroupSaturday.ID_t7 = Team.ID OR GroupSaturday.ID_t8 = Team.ID');
+		$listAdr;
+		$i=0;
+		while($ladr = $adresse -> fetch_array())
+		{
+			$listAdr[$i] = $ladr['Mail'];
+			$i++;
+		}
+		$ajout=count($destCoupe);
+		$i--;
+		for($i;$i!=-1;$i--)
+		{
+			$destCoupe[$ajout]=$listAdr[$i];
+			$ajout++;
+		}	
+	}
+///////////////////////////////////////////////////////////////////////////////
+if($tousDim==true)
+	{
+		$adresse = $db->query('SELECT DISTINCT Mail FROM Personne JOIN Player ON Player.ID_Personne = Personne.ID JOIN Team ON Team.ID_Player1=Player.ID_Personne OR Team.ID_Player2=Player.ID_Personne JOIN GroupSunday ON GroupSunday.ID_t1 = Team.ID OR GroupSunday.ID_t2 = Team.ID OR GroupSunday.ID_t3 = Team.ID OR GroupSunday.ID_t4 = Team.ID OR GroupSunday.ID_t5 = Team.ID OR GroupSunday.ID_t6 = Team.ID OR GroupSunday.ID_t7 = Team.ID OR GroupSunday.ID_t8 = Team.ID');
+		$listAdr;
+		$i=0;
+		while($ladr = $adresse -> fetch_array())
+		{
+			$listAdr[$i] = $ladr['Mail'];
+			$i++;
+		}
+		$ajout=count($destCoupe);
+		$i--;
+		for($i;$i!=-1;$i--)
+		{
+			$destCoupe[$ajout]=$listAdr[$i];
+			$ajout++;
+		}	
+	}
+///////////////////////////////////////////////////////////////////////////////
+//On gère les messages														 //
+///////////////////////////////////////////////////////////////////////////////
+	if($mesLeader==true)
+	{
+		$message = $db->query('SELECT Value FROM GlobalVariables WHERE Name="Message Leader"');
+
+		$sujet =  $db->query('SELECT Value FROM GlobalVariables WHERE Name="Sujet Leader"');
+
+		$adresse = $db->query('SELECT Value FROM GlobalVariables WHERE Name="Adresse du HQ"');
+		
+		//récuperer le message du mail 
+		$listMessage;
+		while($mes = $message->fetch-array())	
+		{
+			$listMessage[0] = $mes['Value'];
+		}
+		//récuperer le sujet du mail
+		$listSujet;
+		while($suj = $sujet->fetch-array())	
+		{
+			$listSujet[0] = $suj['Value'];
+		}
+		//Adresse HQ
+		$listHQ;
+		while($lHQ = $adresse ->fetch-array())
+		{
+			$listHQ[0] = $lHQ['Value'];
+		}
+		$sujet.="\n\n";
+		$sujet .= $listSujet[0];
+		$message.="\n\n";
+		$message .= $listMessage[0];
+		$message.="\n\nPS : Adresse du quartier general : ".$listHQ[0]."\n\n";	
+	}
+///////////////////////////////////////////////////////////////////////////////
+	if($mesNP==true)
+	{
+		$message = $db->query('SELECT Value FROM GlobalVariables WHERE Name="Message Non-Payé"');
+
+		$sujet =  $db->query('SELECT Value FROM GlobalVariables WHERE Name="Sujet Non-Payé"');
+
+		$adresse = $db->query('SELECT Value FROM GlobalVariables WHERE Name="Adresse du HQ"');
+		
+		//récuperer le message du mail 
+		$listMessage;
+		while($mes = $message->fetch-array())	
+		{
+			$listMessage[0] = $mes['Value'];
+		}
+		//récuperer le sujet du mail
+		$listSujet;
+		while($suj = $sujet->fetch-array())	
+		{
+			$listSujet[0] = $suj['Value'];
+		}
+		//Adresse HQ
+		$listHQ;
+		while($lHQ = $adresse ->fetch-array())
+		{
+			$listHQ[0] = $lHQ['Value'];
+		}
+		$sujet.="\n\n";
+		$sujet .= $listSujet[0];
+		$message.="\n\n";
+		$message .= $listMessage[0];
+		$message.="\n\nPS : Adresse du quartier general : ".$listHQ[0]."\n\n";	
+	}
+///////////////////////////////////////////////////////////////////////////////
+	if($mesTous==true)
+	{
+		$message = $db->query('SELECT Value FROM GlobalVariables WHERE Name="Message à tous"');
+
+		$sujet =  $db->query('SELECT Value FROM GlobalVariables WHERE Name="Sujet à tous"');
+
+		$adresse = $db->query('SELECT Value FROM GlobalVariables WHERE Name="Adresse du HQ"');
+		
+		//récuperer le message du mail 
+		$listMessage;
+		while($mes = $message->fetch-array())	
+		{
+			$listMessage[0] = $mes['Value'];
+		}
+		//récuperer le sujet du mail
+		$listSujet;
+		while($suj = $sujet->fetch-array())	
+		{
+			$listSujet[0] = $suj['Value'];
+		}
+		//Adresse HQ
+		$listHQ;
+		while($lHQ = $adresse ->fetch-array())
+		{
+			$listHQ[0] = $lHQ['Value'];
+		}
+		$sujet.="\n\n";
+		$sujet .= $listSujet[0];
+		$message.="\n\n";
+		$message .= $listMessage[0];
+		$message.="\n\nPS : Adresse du quartier general : ".$listHQ[0]."\n\n";	
+	}
+///////////////////////////////////////////////////////////////////////////////	
+	
+//Envoi du mail	
+sendMail($destCoupe, $message, $sujet);
+header('Location: contact.php');	
+?>
---
+++ src/staff/pages/mailLeader.php
@@ -0,0 +1,199 @@
+<?php
+include_once('php/BDD.php');
+include "../../mail/mail_helper.php";
+
+$db = BDconnect();
+
+$gsSam = $db->query('SELECT GroupSaturday.ID as Gid FROM GroupSaturday');
+
+$groupsDim = $db->query('SELECT GroupSunday.ID as Gid FROM GroupSunday');
+
+$message = $db->query('SELECT Value FROM GlobalVariables WHERE Name="Message Leader"');
+
+$sujet =  $db->query('SELECT Value FROM GlobalVariables WHERE Name="Sujet Leader"');
+
+$adresse = $db->query('SELECT Value FROM GlobalVariables WHERE Name="Adresse du HQ"');
+
+//récuperer le message du mail 
+$listMessage;
+while($mes = $message->fetch-array())	
+{
+	$listMessage[0] = $mes['Value'];
+}
+
+//récuperer le sujet du mail
+$listSujet;
+while($suj = $sujet->fetch-array())	
+{
+	$listSujet[0] = $suj['Value'];
+}
+
+//Adresse HQ
+$listHQ;
+while($lHQ = $adresse ->fetch-array())
+{
+	$listHQ[0] = $lHQ['Value'];
+}
+
+//Email à envoyé au leader des groupes du Samedi
+$flagSam=0;
+$i=0;
+$arraySam;
+while ($gSam = $gsSam->fetch_array())
+{
+	$arraySam[$i] = $gSam['Gid'];
+	$i++;
+}
+$i--;
+While($i!=-1)
+{
+if(ISSET($_POST["leaderMail_$arraySam[$i]"]))
+	{
+		$j=0;
+		$listAdress;
+		$mailsSam = $db -> query("SELECT DISTINCT Mail FROM Personne JOIN Player ON Player.ID_Personne = Personne.ID JOIN Team ON Team.ID_Player1=Player.ID_Personne OR Team.ID_Player2=Player.ID_Personne JOIN GroupSaturday ON GroupSaturday.ID_t1 = Team.ID OR GroupSaturday.ID_t2 = Team.ID OR GroupSaturday.ID_t3 = Team.ID OR GroupSaturday.ID_t4 = Team.ID OR GroupSaturday.ID_t5 = Team.ID OR GroupSaturday.ID_t6 = Team.ID OR GroupSaturday.ID_t7 = Team.ID OR GroupSaturday.ID_t8 = Team.ID WHERE Player.IsLeader=1 AND GroupSaturday.ID=".$arraySam[$i]);
+		$y=0;
+		$listRespFirst;
+		$listRespLast;
+		$listRespPhone;
+		$listRespGSM;
+		$listRespMail;
+		$respCat= $db->query("SELECT DISTINCT Personne.FirstName, Personne.LastName, Personne.PhoneNumber, Personne.GSMNumber, Personne.Mail FROM Personne JOIN Staff ON Staff.ID_Personne = Personne.ID JOIN Categorie ON Staff.ID_Cat = Categorie.ID JOIN Team ON Team.ID_Cat = Categorie.ID JOIN GroupSaturday ON GroupSaturday.ID_t1 = Team.ID WHERE GroupSaturday.ID=".$arraySam[$i]);
+		$listCat;
+		$numeroCat= $db->query("SELECT Categorie.ID FROM Categorie JOIN Team ON Team.ID_Cat = Categorie.ID JOIN GroupSaturday ON GroupSaturday.ID_t1 = Team.ID WHERE GroupSaturday.ID=".$arraySam[$i]);
+		while($nCat = $numeroCat->fetch_array())
+		{
+			$listCat[0]=$nCat['ID'];
+		}		
+		while($mailSam = $mailsSam ->fetch_array())
+		{
+			$listAdress[$j]=$mailSam['Mail'];
+			$j++;
+		}
+		while($respC = $respCat->fetch_array())
+		{
+			$listRespFirst[$y]=$respC['FirstName'];
+			$listRespLast[$y]=$respC['LastName'];
+			$listRespPhone[$y]=$respC['PhoneNumber'];
+			$listRespGSM[$y]=$respC['GSMNumber'];
+			$listRespMail[$y]=$respC['Mail'];
+			$y++;
+		}
+		$i--;
+		$flagSam=1;
+		break;
+	}
+	else{
+		$i--;
+	}
+}
+//On test si on est samedi
+if($flagSam==1) //Si oui on envoie le mail à ceux du groupe cliqué
+{
+	$to[0]="";
+	$h=0;
+for($j--;$j!=-1;$j--)
+{
+	$to[$h] = $listAdress[$j]; 
+	$h++;
+}
+$email_subject = $listSujet[0];
+$email_body = $listMessage[0];
+$psResponsable="\n\nCoordonnees des responsables : ";
+for($y--;$y!=-1;$y--)
+{
+	$psResponsable.="Prenom : ".$listRespFirst[$y]."\n\n";
+	$psResponsable.="Nom : ".$listRespLast[$y]."\n\n";
+	$psResponsable.="Numero de telephone : ".$listRespPhone[$y]."\n\n";
+	$psResponsable.="Numero de GSM : ".$listRespGSM[$y]."\n\n";
+	$psResponsable.="Adresse Email : ".$listRespMail[$y]."\n\n";
+	$psResponsable.="----------------\n\n";
+}
+$email_body.=$psResponsable;
+$email_body.="PS : Adresse du quartier general : ".$listHQ[0]."\n\n";
+sendMail($to, $email_body, $email_subject);
+header('Location: group.php?jour=sam&cat='.$listCat[0]);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+//Email à envoyer au Leader des groupes du dimanche
+else //Si on est dimanche
+{
+	$k=0;
+	$arrayDim;
+	while ($gDim = $groupsDim->fetch_array())
+	{
+		$arrayDim[$k] = $gDim['Gid'];
+		$k++;
+	}
+	$k--;
+	While($k!=-1)
+	{
+		if(ISSET($_POST["leaderMail_$arrayDim[$k]"]))
+		{
+			$t=0;
+			$listAdressDim;
+			$mailsDim = $db -> query("SELECT DISTINCT Mail FROM Personne JOIN Player ON Player.ID_Personne = Personne.ID JOIN Team ON Team.ID_Player1=Player.ID_Personne OR Team.ID_Player2=Player.ID_Personne JOIN GroupSunday ON GroupSunday.ID_t1 = Team.ID OR GroupSunday.ID_t2 = Team.ID OR GroupSunday.ID_t3 = Team.ID OR GroupSunday.ID_t4 = Team.ID OR GroupSunday.ID_t5 = Team.ID OR GroupSunday.ID_t6 = Team.ID OR GroupSunday.ID_t7 = Team.ID OR GroupSunday.ID_t8 = Team.ID WHERE Player.IsLeader=1 AND GroupSunday.ID=".$arrayDim[$k]); 
+			$y=0;
+			$listRespFirst;
+			$listRespLast;
+			$listRespPhone;
+			$listRespGSM;
+			$listRespMail;
+			$respCat= $db->query("SELECT DISTINCT Personne.FirstName, Personne.LastName, Personne.PhoneNumber, Personne.GSMNumber, Personne.Mail FROM Personne JOIN Staff ON Staff.ID_Personne = Personne.ID JOIN Categorie ON Staff.ID_Cat = Categorie.ID JOIN Team ON Team.ID_Cat = Categorie.ID JOIN GroupSunday ON GroupSunday.ID_t1 = Team.ID WHERE GroupSunday.ID=".$arrayDim[$k]);
+			$listCat;
+			$numeroCat= $db->query("SELECT Categorie.ID FROM Categorie JOIN Team ON Team.ID_Cat = Categorie.ID JOIN GroupSunday ON GroupSunday.ID_t1 = Team.ID WHERE GroupSunday.ID=".$arrayDim[$k]);
+			while($nCat = $numeroCat->fetch_array())
+			{
+				$listCat[0]=$nCat['ID'];
+			}
+			
+			while($mailDim = $mailsDim ->fetch_array())
+			{
+				$listAdressDim[$t]=$mailDim['Mail'];
+				$t++;
+			}
+			while($respC = $respCat->fetch_array())
+			{
+			$listRespFirst[$y]=$respC['FirstName'];
+			$listRespLast[$y]=$respC['LastName'];
+			$listRespPhone[$y]=$respC['PhoneNumber'];
+			$listRespGSM[$y]=$respC['GSMNumber'];
+			$listRespMail[$y]=$respC['Mail'];
+			$y++;
+			}
+			$k--;
+			break;
+		}
+		else
+		{
+			$k--;
+		}
+	}
+	$to[0]="";
+	$h=0;
+	for($t--;$t!=-1;$t--)
+	{
+		$to[$h] = $listAdressDim[$t]; 
+		$h++;
+	}
+	$email_subject = $listSujet[0];
+	$email_body = $listMessage[0];
+	$psResponsable="\n\nCoordonnees des responsables : ";
+	for($y--;$y!=-1;$y--)
+	{
+		$psResponsable.="Prenom : ".$listRespFirst[$y]."\n\n";
+		$psResponsable.="Nom : ".$listRespLast[$y]."\n\n";
+		$psResponsable.="Numero de telephone : ".$listRespPhone[$y]."\n\n";
+		$psResponsable.="Numero de GSM : ".$listRespGSM[$y]."\n\n";
+		$psResponsable.="Adresse Email : ".$listRespMail[$y]."\n\n";
+		$psResponsable.="----------------\n\n";
+	}
+	$email_body.=$psResponsable;
+	$email_body.="PS : Adresse du quartier general : ".$listHQ[0]."\n\n";
+	sendMail($to, $email_body, $email_subject);	
+	header('Location: group.php?jour=dim&cat='.$listCat[0]);
+}
+?>
