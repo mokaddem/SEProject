d46977056d494f956dfd0220049385ad97f82003
System Administrator <root@MacBook-Pro-de-Florian.local>
2015-12-07 13:29:02 +0100

    Début Liste Variables Globales

---
+++ src/staff/pages/listVarGlobal.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Page affichant tous les extras existants -->
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Staff - Charles de Lorraine - Liste</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+
+    <!-- DataTables CSS -->
+    <link href="../bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css" rel="stylesheet">
+
+    <!-- DataTables Responsive CSS -->
+    <link href="../bower_components/datatables-responsive/css/dataTables.responsive.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <?php
+            include("./html/header.php");
+        ?>
+
+            <div id="page-wrapper" style="background : url(../../images/staff-back.jpg) 0 0 fixed;">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">Liste des variables globales
+                        <a class="btn btn-default btn-outline pull-right" href="./varGlobal.php"> Créer</a>
+                        </h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+
+                <?php
+                if (array_key_exists("error", $_GET)) {?>
+                    <p class="alert alert-danger"> Vous ne pouvez pas supprimer cette équipe car elle est utilisée ailleurs</p>
+                <?php }
+                ?>
+
+                <!-- /.row -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-default">
+                            <!-- <div class="panel-heading">
+                            DataTables Advanced Tables
+                        </div> -->
+                            <!-- /.panel-heading -->
+                            <div class="panel-body">
+                                <div class="dataTable_wrapper">
+                                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                        <thead>
+                                            <tr>
+                                                <?php
+                                                    require_once("./php/inc/list-function.inc");
+                                                    $titreDonnees = getTitreTable("varGlobal");
+                                                    foreach ($titreDonnees as $titreD){ ?>
+                                                    <th>
+                                                        <?=$titreD?>
+                                                    </th>
+                                                <?php } ?>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php require_once("./php/inc/list-varGlobal.inc");
+                                            dbAndPrint();
+                                            ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <!-- /.panel-body -->
+                        </div>
+                        <!-- /.panel -->
+                    </div>
+                    <!-- /.col-lg-6 -->
+                </div>
+                <!-- /.row -->
+                <!-- Modal -->
+                <div id="myModal" class="modal fade" role="dialog">
+                </div>
+
+            </div>
+            <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="../bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- DataTables JavaScript -->
+    <script src="../bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
+    <script src="../bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../dist/js/sb-admin-2.js"></script>
+
+    <!-- Page-Level Demo Scripts - Tables - Use for reference -->
+    <script>
+        $(document).ready(function () {
+            $('#dataTables-example').DataTable({
+                responsive: true
+            });
+        });
+    </script>
+    <script type="text/javascript">
+        // On click, get html content from url and update the corresponding modal
+        $("[data-toggle='modal']").on("click", function (event) {
+            event.preventDefault();
+            var url = $(this).attr('data-url');
+            var modal_id = $(this).attr('data-target');
+            $.get(url, function (data) {
+                $(modal_id).html(data);
+            });
+        });
+    </script>
+</body>
+
+</html>
