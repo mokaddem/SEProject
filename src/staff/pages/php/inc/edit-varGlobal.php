1a4f602c7a8a430973c8390bf341c72413613871
System Administrator <root@MacBook-Pro-de-Florian.local>
2015-12-08 00:19:14 +0100

    add fonctionne, pas le show ni edit

--- src/staff/pages/php/inc/edit-varGlobal.php
+++ src/staff/pages/php/inc/edit-varGlobal.php
@@ -11,13 +11,13 @@ require_once('../add-new-history.php');
 //$db = new mysqli($database_host, $database_user, $database_pass, $database_db);
 $db = BDconnect();
 $ID	= $_GET['Name'];
-$Description = utf8_decode($_GET['Value']);
+$Description = utf8_encode($_GET['value']);
 
 
 $reponse = $db->query("UPDATE SEProjectC.GlobalVariables SET Value = \"".$Description."\", Name=\"" .$ID." WHERE ".$ID."=GlobalVariables.Name");
 
 addHistory($ID, "Variables Globales", "Edition");
 
-header("Location: ../../list.php?type=varGlobal");
+header("Location: ../../listVarGlobal.php");
 
 ?>
