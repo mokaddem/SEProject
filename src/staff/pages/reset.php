f5da83c0f4a28b514943e41ef3d3c6fe70afdaa9
antoinerollin <antoine.rollin26@free.fr>
2015-12-09 03:02:24 +0100

    reset correct

--- src/staff/pages/reset.php
+++ src/staff/pages/reset.php
@@ -48,7 +48,7 @@
 
                             <label><p>Attention, le fait de réinitialiser le tournoi va supprimer toutes les poules et le Knock-Off. Cette action est irréversible.</p></label>
                             <div >
-                                <button type="submit" name="submit" id="submit" class="btn btn-danger pull-center"><i class="fa fa-refresh"></i> J'ai pris connaissances des risques</button>
+                                <button type="submit" name="submit" id="submit" class="btn btn-danger btn-outline pull-center"><i class="fa fa-refresh"></i> J'ai pris connaissances des risques</button>
                             </div>
 
                         </div>
